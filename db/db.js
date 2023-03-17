e9df6935a8774c3ef64c5ceec4acd74b411fed8e
David Rivera <djro1605@gmail.com>
2023-03-17 05:01:30 -0600

    Add db configuration changes

--- db/db.js
+++ db/db.js
@@ -2,6 +2,7 @@
 const knex = require("knex")({
   client: "pg",
   connection: process.env.PG_CONNECTION_STRING,
+  pool: { min: 0, max: 10, acquireTimeoutMillis: 300000, }
 });
 
 module.exports = knex;
